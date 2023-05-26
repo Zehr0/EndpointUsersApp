6923ccf45df8e84de6304b05d8136863524a155e
zero <hilahiko@gmail.com>
2023-05-26 17:05:12 +0100

    attached adapter

--- app/src/main/java/com/hilary/endpointusersapp/MainActivity.java
+++ app/src/main/java/com/hilary/endpointusersapp/MainActivity.java
@@ -7,6 +7,9 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.google.gson.JsonObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -48,13 +51,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void fetchUsers() {
         String url = "https://api.slingacademy.com/v1/sample-data/users";
+        System.out.println(url);
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, new Response.Listener<JSONObject>() {
             @Override
-            public void onResponse(JSONArray response) {
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        JSONObject jsonObject = response.getJSONObject(i);
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("users");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
 
                         String firstName = jsonObject.getString("first_name");
                         String lastName = jsonObject.getString("last_name");
@@ -62,13 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
                         User user = new User(firstName, lastName, email);
                         userList.add(user);
-                    }catch (JSONException e){
-                        e.printStackTrace();
                     }
 
-                    UserAdapter adapter = new UserAdapter(MainActivity.this, userList);
-                    userRecyclerView.setAdapter(adapter);
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
                 }
+                UserAdapter adapter = new UserAdapter(MainActivity.this, userList);
+                userRecyclerView.setAdapter(adapter);
             }
         }, new Response.ErrorListener() {
             @Override
@@ -77,6 +82,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        requestQueue.add(jsonArrayRequest);
+        requestQueue.add(jsonObjectRequest);
     }
 }
